af5044bb4d9089624f7a2363d56464c4747a8e85
Brittney Ramirez <brittneyramirez79@gmail.com>
2020-01-03 19:04:30 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,5 +4,14 @@
 # or whitespace (' ').
 
 
-class EmailParser 
-  def initialize("email")
+class EmailAddressParser 
+  attr_reader:emails
+  
+  def initialize("emails")
+    @emails = emails
+  end
+  
+  def parse
+    emails,split(/,|/.uniq)
+  end
+end
